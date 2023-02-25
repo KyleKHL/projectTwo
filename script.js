7beaf4a8241bccfd3059d7f6aece24e4999718ec
Kyle Lau <kyle.lau15@gmail.com>
2023-02-25 16:47:37 -0500

    added title

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import firebaseInfo from "./firebase.js";
 
 import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
-
+// initialize db content
 const database = getDatabase(firebaseInfo);
-
+// create reference to db content
 const dbref = ref(database);
